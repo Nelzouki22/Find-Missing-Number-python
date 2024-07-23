78142026b5578f008d051280c6fbfe580267d6a8
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-07-22 20:45:39 -0400

    Add project files.

---
+++ Find_Missing_Number__python.py
@@ -0,0 +1,15 @@
+﻿def find_missing_number(nums):
+    n = len(nums)
+    total = (n + 1) * (n + 2) // 2  # Sum of first n+1 natural numbers
+
+    for num in nums:
+        total -= num
+
+    return total
+
+# مثال على مصفوفة بها رقم مفقود
+nums = [1, 2, 4, 5, 6]
+missing_number = find_missing_number(nums)
+
+print(f"The missing number is: {missing_number}")
+
